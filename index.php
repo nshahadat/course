a8858bf9512b432ca83d9a5b57a464b7843017a9
Arnob <arnobm97@gmail.com>
2023-05-30 14:08:44 +0600

    navbar added

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
 include "./includes/header.php";
 ?>
-
+  <header>
+        <a href="#" class="logo"><img src="/coursereg/media/images/logo.png" class="nav-logo"></a>
+        <ul>
+            <li><a href="/coursereg/index.php">Home</a></li>
+            <li>
+            <div class="dropdown">
+                <button class="dropbtn">Dropdown
+                </button>
+                <div class="dropdown-content">
+                <a href="#">Student Login</a>
+                <a href="#">Student Signup</a>
+                <a href="#">Accounts Login</a>
+                <a href="#">Advisor Login</a>
+                <a href="#">Registrar Login</a>
+                <a href="#">Admin</a>
+                </div>
+            </div>
+            </li>
+            <li><a href="/coursereg/courses/courses.php">Courses</a></li>
+            <li><a href="/coursereg/login.php" target="_blank">Login</a></li>
+        </ul>
+    </header>
 <section class="hero-section">
     <div class="left-hero">
         <img src="./media/student.png" id="left-hero-img" class="left-hero-img">
@@ -12,7 +33,42 @@ include "./includes/header.php";
 </section>
 
 <div class="color-design"></div>
+<h1 class="g-name">Gallery</h1>
+<div class="gallery">
+    <div class="img-container">
+            <img src="media/Example.jpg"alt="Mountain"data-orginal="media/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="media/Example.jpg"alt="Mountain"data-orginal="media/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="media/Example.jpg"alt="Mountain"data-orginal="media/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="media/Example.jpg"alt="Mountain"data-orginal="media/Examplebig.jpg">
+    </div> 
 
+</div>
+<h1 class="g-name">Gallery</h1>
+<div class="gallery">
+        <div class="img-container">
+            <img src="media/Example.jpg"alt="Mountain"data-orginal="media/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="media/Example.jpg"alt="Mountain"data-orginal="media/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="media/Example.jpg"alt="Mountain"data-orginal="media/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="media/Example.jpg"alt="Mountain"data-orginal="media/Examplebig.jpg">
+    </div> 
+    </div>
+</div>
+<div class="modal">
+    <img src="media/Examplebig.jpg" alt=""class="full-img">
+    <p class="caption">mountain</p>
+</div>
 <?php
 include "./includes/footer.php";
 ?>
