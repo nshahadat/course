4a652c0de375fe196460241ccbe12a07898240bd
beshahadat <shahadatnayeem23@gmail.com>
2023-05-31 16:12:51 +0600

    final

--- account/account_feed.php
+++ account/account_feed.php
@@ -12,22 +12,25 @@ session_start();
                 <?= $_SESSION['username'] ?>
             </button>
         </div>
-        <a href="..logout.php">
+        <a href="../logout.php">
             <div class="logout-btn">
                 <button>logout</button>
             </div>
         </a>
     </div>
     <div class="feed-container">
-       <div class="box">
-       <img src="../media/check.avif" alt=""class="feed-img">
-       <button class="extra-btn" onclick=gotoapplies()>Check New Applies</button></div>
-       <div class="box">
-       <img src="../media/h.jpg" alt=""class="feed-img"> 
-       <button class="extra-btn" onclick=gotohistory()>Confirmation History</button></div>
-       <div class="box">
-       <img src="../media/account.avif" alt=""class="feed-img">
-       <button class="extra-btn" onclick=gotoaccounts()>Check Accounts</button></div>
+        <div class="box">
+            <img src="../media/check.avif" alt="" class="feed-img">
+            <button class="extra-btn" onclick=gotoapplies()>Check New Applies</button>
+        </div>
+        <div class="box">
+            <img src="../media/h.jpg" alt="" class="feed-img">
+            <button class="extra-btn" onclick=gotohistory()>Confirmation History</button>
+        </div>
+        <div class="box">
+            <img src="../media/account.avif" alt="" class="feed-img">
+            <button class="extra-btn" onclick=gotoaccounts()>Check Accounts</button>
+        </div>
     </div>
 </div>
 
